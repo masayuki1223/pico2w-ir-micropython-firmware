4e222b546a320eb1f4f752e5608673e600d98528
masayuki1223 <gu.mechasys.0876@gmail.com>
2026-06-04 20:27:38 +0900

    Create infrared_ext.h

---
+++ ir/src/infrared_ext.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025 Indoor Corgi
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef INFRARED_H
+#define INFRARED_H
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+
+// -----------------
+// Configurations
+
+// 可変ピンとして外部から設定する
+extern uint8_t infrared_send_pin;      // 送信ピン番号
+extern uint8_t infrared_receive_pin;   // 受信ピン番号
+
+// 受信時のしきい値[us]. この時間以上ONまたはOFFが続く場合は受信終了
+#define INFRARED_RECEIVE_THRESHOLD 100000
+
+#define infrared_delay(x) sleep_ms(x)  // xミリ秒待機
+
+// -----------------
+
+bool infrared_send_init();
+void infrared_send_program_init(PIO pio, uint sm, uint offset, uint pin);
+void infrared_send_deinit();
+void infrared_send(uint32_t* data, uint length, bool wait_complete);
+
+bool infrared_receive_init();
+void infrared_receive_program_init(PIO pio, uint sm, uint offset, uint pin);
+void infrared_receive_deinit();
+int infrared_receive_blocking(uint32_t* data, uint length);
+
+#endif
